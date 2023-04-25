8e098520b04c39897c3050ac95cacaa1e2f2d5d6
PeterWalchhofer <peter.walchhofer@liwest.at>
2023-04-25 15:30:08 +0200

    update data

--- data/neo4j_bulk/neo4j_import.sh
+++ data/neo4j_bulk/neo4j_import.sh
@@ -2,8 +2,3 @@ neo4j-admin import --id-type=STRING --database=graph.db \
 	--multiline-fields=true \
 	--relationships=_links.csv \
 	--nodes=_nodes.csv \
-	--relationships=Membership.csv \
-	--nodes=Person.csv \
-	--relationships=Ownership.csv \
-	--nodes=Company.csv \
-	--nodes=Organization.csv \
